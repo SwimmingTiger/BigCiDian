dedfca64bba68438c4edd95d1276a1b3086afd28
SwimmingTiger <hu60.cn@gmail.com>
2022-07-04 22:09:56 +0800

    add a script to replace tone marks to IPA tone letters

---
+++ pitch_to_ipa.sh
@@ -0,0 +1,7 @@
+# IPA Tone letter: <https://en.wikipedia.org/wiki/Tone_letter#Numerical_values>
+
+sed -i 's/_1\b/˥/g' lexicon.txt phoneset.list
+sed -i 's/_2\b/˧˥/g' lexicon.txt phoneset.list
+sed -i 's/_3\b/˨˩˦/g' lexicon.txt phoneset.list
+sed -i 's/_4\b/˥˩/g' lexicon.txt phoneset.list
+sed -i 's/_0\b/˩/g' lexicon.txt phoneset.list
